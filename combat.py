4a572a1bd87610ac7331159edfb7c3425eb4fc2a
said-kheloufi <146715579+said-kheloufi@users.noreply.github.com>
2024-01-09 11:41:17 +0100

    combat.py

--- combat.py
+++ combat.py
@@ -1,16 +1,32 @@
 class Combat:
     def __init__(self, attacker, defender):
-        type_multiplier = {"Eau" : {"Terre":0.5, "Feu":2.0, 'Normal':1.0}, "Feu":{"Eau":0.5, "Terre":2.0, "Normal":1.0}, "Terre":{"Feu":0.5, "Eau":2.0}, "Normal":1.0}
-        damage = attacker.attack * type_multiplier.get(attacker.type, {}).get(defender.type, 1.0)
-        return int(damage)
-    
-    def apply_damage(defender, damage):
+        self.attacker = attacker
+        self.defender = defender
+
+    def get_damage_multiplier(self, attacker_type, defender_type):
+        type_multiplier = {"Eau": {"Terre": 0.5, "Feu": 2.0, "Normal": 1.0}, "Feu": {"Eau": 0.5, "Terre": 2.0, "Normal": 1.0}, "Terre": {"Feu": 0.5, "Eau": 2.0}, "Normal": 1.0}
+        return type_multiplier.get(attacker_type, {}).get(defender_type, 1.0)
+
+    def inflict_damage(self, defender, damage):
         defender.hp -= max(damage - defender.defense, 0)
 
-    def determine_winner(pokemon, opponent):
+    def get_winner(self, pokemon, opponent):
         if pokemon.hp <= 0:
             return opponent.name
         elif opponent.hp <= 0:
             return pokemon.name
         else:
             return None
+
+    def get_loser(self, pokemon, opponent):
+        if pokemon.hp <= 0:
+            return pokemon.name
+        elif opponent.hp <= 0:
+            return opponent.name
+        else:
+            return None
+       
+    def register_pokemon(self, pokemon):
+        pokedex = []
+        pokedex.append(pokemon)
+        print(f"{pokemon.name} a été ajouté à votre Pokédex!")
