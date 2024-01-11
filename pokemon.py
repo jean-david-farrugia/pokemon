1e6662a6bee6d11f0f82e41482d6758b47b35aa7
said-kheloufi <146715579+said-kheloufi@users.noreply.github.com>
2024-01-11 11:11:14 +0100

    pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,20 +1,24 @@
 class Pokemon:
+    # Initialisation de la classe Pokemon
     def __init__(self, name, hp, level, attack_power, defense, type, evolution_level=None, evolution_name=None):
-        self.name = name
-        self.hp = hp
-        self.level = level
-        self.attack_power = attack_power
-        self.defense = defense
-        self.type = type
-        self.evolution_level = evolution_level
-        self.evolution_name = evolution_name
+        self.name = name  # Nom du Pokemon
+        self.hp = hp  # Points de vie du Pokemon
+        self.level = level  # Niveau du Pokemon
+        self.attack_power = attack_power  # Puissance d'attaque du Pokemon
+        self.defense = defense  # Défense du Pokemon
+        self.type = type  # Type du Pokemon (par exemple, Feu, Eau, Plante, etc.)
+        self.evolution_level = evolution_level  # Niveau auquel le Pokemon évolue
+        self.evolution_name = evolution_name  # Nom de l'évolution du Pokemon
 
+    # Méthode pour faire évoluer le Pokemon
     def evolve(self):
+        # Vérifie si le Pokemon a un niveau d'évolution et si son niveau actuel est suffisant pour évoluer
         if self.evolution_level is not None and self.level >= self.evolution_level:
             print(f"{self.name} est en train d'évoluer !")
-            self.name = self.evolution_name
-            self.attack_power += 10  # Increase stats upon evolution
-            self.defense += 10
+            self.name = self.evolution_name  # Change le nom du Pokemon pour son évolution
+            self.attack_power += 10  # Augmente la puissance d'attaque du Pokemon lors de l'évolution
+            self.defense += 10  # Augmente la défense du Pokemon lors de l'évolution
             print(f"Félicitations ! Votre {self.name} a évolué !")
         else:
+            # Si le Pokemon n'est pas prêt à évoluer, affiche un message
             print(f"{self.name} n'est pas encore prêt à évoluer.")
