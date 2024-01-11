9245dc90d0f6f5f90364f5eade80e63eda13e062
said-kheloufi <146715579+said-kheloufi@users.noreply.github.com>
2024-01-11 09:21:50 +0100

    combat.py

--- combat.py
+++ combat.py
@@ -7,33 +7,32 @@ class Combat:
         type_multiplier = {"Eau": {"Terre": 0.5, "Feu": 2.0, "Normal": 1.0}, "Feu": {"Eau": 0.5, "Terre": 2.0, "Normal": 1.0}, "Terre": {"Feu": 0.5, "Eau": 2.0}, "Normal": 1.0}
         return type_multiplier.get(attacker_type, {}).get(defender_type, 1.0)
 
-    def inflict_damage(self, defender, damage):
-        defender.hp -= max(damage - defender.defense, 0)
+    def inflict_damage(self):
+        damage_multiplier = self.get_damage_multiplier(self.attacker.type, self.defender.type)
+        damage = self.attacker.puissance_attaque * damage_multiplier
+        self.defender.points_de_vie -= max(damage - self.defender.defense, 0)
 
-    def get_winner(self, pokemon, opponent):
-        if pokemon.hp <= 0:
-            return opponent.name
-        elif opponent.hp <= 0:
-            return pokemon.name
+    def get_winner(self):
+        if self.attacker.points_de_vie <= 0:
+            return self.defender.nom
+        elif self.defender.points_de_vie <= 0:
+            return self.attacker.nom
         else:
             return None
 
-    def get_loser(self, pokemon, opponent):
-        if pokemon.hp <= 0:
-            return pokemon.name
-        elif opponent.hp <= 0:
-            return opponent.name
+    def get_loser(self):
+        if self.attacker.points_de_vie <= 0:
+            return self.attacker.nom
+        elif self.defender.points_de_vie <= 0:
+            return self.defender.nom
         else:
             return None
-       
+
     def register_pokemon(self, pokemon):
         pokedex = []
         pokedex.append(pokemon)
-        print(f"{pokemon.name} a été ajouté à votre Pokédex!")
+        print(f"{pokemon.nom} a été ajouté à votre Pokédex!")
         
-    def get_opponent_attack_power(self, opponent):
-        opponent_type = opponent.type
-        opponent_attack_power = opponent.attack_power
-        attacker_type = self.attacker.type
-        damage_multiplier = self.get_damage_multiplier(attacker_type, opponent_type)
-        return opponent_attack_power * damage_multiplier
+    def get_opponent_attack_power(self):
+        damage_multiplier = self.get_damage_multiplier(self.attacker.type, self.defender.type)
+        return self.defender.puissance_attaque * damage_multiplier
