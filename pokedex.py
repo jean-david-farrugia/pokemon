c3c9df047b2f7f54f0e2dbb0b280cdd483a2d584
said-kheloufi <146715579+said-kheloufi@users.noreply.github.com>
2024-01-26 11:37:18 +0100

    pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -1,42 +1,53 @@
+import pygame
 import json
 
+
 class Pokedex:
-    # Initialisation de la classe Pokedex
-    def __init__(self, filename="jsonpokedex.json"):
-        self.filename = filename  # Nom du fichier contenant le Pokedex
-        self.pokedex = self.load_pokedex()  # Charge le Pokedex à partir du fichier
+    
+    def __init__(self, pokemon_data_file, rival_data_file):
+        self.pokemon_data_file = pokemon_data_file
+        self.rival_data_file = rival_data_file
 
-    # Méthode pour charger le Pokedex à partir du fichier
-    def load_pokedex(self):
+    def update_rival_data(self, defeated_rival_name, new_sprite_path):
         try:
-            with open(self.filename, "r") as file:
-                return json.load(file)  # Charge le Pokedex à partir du fichier JSON
-        except FileNotFoundError:
-            return {}  # Si le fichier n'existe pas, retourne un Pokedex vide
+            with open(self.rival_data_file) as rival_file:
+                rival_data = json.load(rival_file)
 
-    # Méthode pour sauvegarder le Pokedex dans le fichier
-    def save_pokedex(self):
-        with open(self.filename, "w") as file:
-            json.dump(self.pokedex, file)  # Sauvegarde le Pokedex dans le fichier JSON
+            rival_names = [pokemon['name'] for pokemon in rival_data]
+            if defeated_rival_name not in rival_names:
+                new_rival_data = {
+                    "name": defeated_rival_name,
+                    "sprites": {
+                        "front_default": new_sprite_path
+                    }
+                }
+                rival_data.append(new_rival_data)
 
-    # Méthode pour enregistrer un Pokemon dans le Pokedex
-    def register_pokemon(self, pokemon):
-        if pokemon.name not in self.pokedex:  # Vérifie si le Pokemon n'est pas déjà dans le Pokedex
-            # Ajoute le Pokemon au Pokedex
-            self.pokedex[pokemon.name] = {
-                "name": pokemon.name,
-                "hp": pokemon.hp,
-                "level": pokemon.level,
-                "attack_power": pokemon.attack_power,
-                "defense": pokemon.defense,
-                "type": pokemon.type
-            }
-            print(f"{pokemon.name} a été ajouté à votre Pokédex!")  # Affiche un message de confirmation
-            self.save_pokedex()  # Sauvegarde le Pokedex
-        else:
-            print(f"{pokemon.name} est déjà dans votre Pokédex.")  # Affiche un message si le Pokemon est déjà dans le Pokedex
+            with open(self.rival_data_file, 'w') as updated_rival_file:
+                json.dump(rival_data, updated_rival_file, indent=2)
 
-    # Méthode pour afficher le Pokedex
-    def display_pokedex(self):
-        for pokemon in self.pokedex.values():  # Parcourt tous les Pokemon dans le Pokedex
-            print(pokemon)  # Affiche les informations du Pokemon
+            print(f"Rival data updated for {defeated_rival_name}.")
+        except Exception as e:
+            print(f"Error updating rival data: {e}")
+
+
+    def display(self, game):
+        with open(self.rival_data_file) as rival_file:
+            rival_data = json.load(rival_file)
+        for i, pokemon in enumerate(rival_data):
+            sprite_path = pokemon['sprites']['front_default']
+            sprite_image = pygame.image.load(sprite_path)
+
+            new_size = (150, 150)
+
+            sprite_image = pygame.transform.scale(sprite_image, new_size)
+
+            sprite_rect = sprite_image.get_rect()
+            sprite_rect.x = 10 + i * 100  
+            sprite_rect.y = 30
+            game.blit(sprite_image, sprite_rect)
+
+            font = pygame.font.Font(pygame.font.get_default_font(), 16)
+            text = font.render(pokemon['name'], True, (0, 0, 0))
+            text_rect = text.get_rect(center=(sprite_rect.centerx, sprite_rect.bottom + 20))
+            game.blit(text, text_rect)
